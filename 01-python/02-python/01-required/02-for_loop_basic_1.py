89c8f1a143a67b3ad17d55cd1efc2d9a10db225a
Yashey Mateen <yasheymateen@gmail.com>
2018-11-06 01:00:43 -0500

    added for loop basic assignment

---
+++ 01-python/02-python/01-required/02-for_loop_basic_1.py
@@ -0,0 +1,37 @@
+for count in range (0,151):
+    print(count)
+
+
+for count in range (5, 1000000):
+    print(count*5)
+
+
+for count in range (0,100):
+    if count%5 == 0:
+        print("Coding")
+    if count%10 == 0:
+        print("Dojo")
+    print(count)
+
+
+sum = 0
+for num in range(0,500000):
+    if num%2 == 1:
+        sum+= num
+    print(sum)
+
+
+for count in range (2018,1):
+    if count%2 == 0:
+        print(count)
+    count-=4
+
+
+
+
+
+3,5,1,2
+
+undefined
+
+0,1,2,3
