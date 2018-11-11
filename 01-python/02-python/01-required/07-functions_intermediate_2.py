d8ecfde785821a72dc396720e627a5f978caa6a6
Yashey Mateen <yasheymateen@gmail.com>
2018-11-10 23:41:25 -0500

    added functions intermediate 2 assignment

---
+++ 01-python/02-python/01-required/07-functions_intermediate_2.py
@@ -0,0 +1,114 @@
+1.
+x = [ [5,2,3], [10,8,9] ] 
+students = [
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'}
+]
+sports_directory = {
+    'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
+    'soccer' : ['Messi', 'Ronaldo', 'Rooney']
+}
+z = [ {'x': 10, 'y': 20} ]
+
+How would you change the value 10 in x to 15?  Once you're done x should then be [ [5,2,3], [15,8,9] ].  
+
+x[1][0]=15
+print x
+
+
+
+
+How would you change the last_name of the first student from 'Jordan' to "Bryant"?
+students[0].'last_name' = 'Bryant'
+print students
+
+For the sports_directory, how would you change 'Messi' to 'Andres'?
+sports_directory.'soccer'[0] = 'Andres'
+print sports_directory
+
+For z, how would you change the value 20 to 30?
+z[0].'y' = 30
+print z
+
+
+
+2. Create a function that given a list of dictionaries, it loops through each dictionary in the list and prints each key and the associated value.  For example, given the following list:
+
+students = [
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'},
+     {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+     {'first_name' : 'KB', 'last_name' : 'Tonel'}
+]
+
+iterateDictionary( students ) should output
+
+first_name - Michael, last_name - Jordan
+first_name - John, last_name - Rosales
+first_name - Mark, last_name - Guillen
+first_name - KB, last_name - Tonel
+
+
+def iterateDictionary(students):
+    for i in students:
+        for key in i:
+            print(key, "-", i[key])
+
+iterateDictionary(students)
+
+3. Create a function that given a list of dictionaries and a key name, it outputs the value stored in that key for each dictionary.  For example, iterateDictionary2('first_name', students) should output
+
+Michael
+John
+Mark
+KB
+
+def iterateDictionary2(x, students):
+    x=i[key]
+    for i in students:
+        for key in i:
+            print(i['first_name'])
+
+iterateDictionary2('first_name', students)
+
+
+4.  Say that
+
+dojo = {
+   'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+   'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+}
+
+Create a function that prints the name of each location and also how many locations the Dojo currently has.  Have the function also print the name of each instructor and how many instructors the Dojo currently has.  For example, printDojoInfo(dojo) should output
+
+7 LOCATIONS
+San Jose
+Seattle
+Dallas
+Chicago
+Tulsa
+DC
+Burbank
+    
+8 INSTRUCTORS
+Michael
+Amy
+Eduardo
+Josh
+Graham
+Patrick
+Minh
+Devon
+
+dojo= {
+  ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+  ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+}
+
+locations=dojo[0]
+instructors=dojo[1]
+
+def printDojoinfo(dojo):
+  for key in dojo:
+    for i in key:
+      print(len(locations), locations[i])
